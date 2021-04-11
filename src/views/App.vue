0d14b62259c22c68e6e33fe073a01b5d2a631605
Faiz Ainur Rofiq <cavadevop@gmail.com>
2021-04-11 22:29:07 +0700

    fix bug

--- src/views/App.vue
+++ src/views/App.vue
@@ -62,8 +62,8 @@ export default {
   name: 'App',
   data() {
     return {
-      jwtToken: '',
-      // jwtToken: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVkZW50aWFsVHlwZSI6ImJhc2ljIiwiZXhwIjoxNjE4MDg0MTUyLCJpYXQiOjE2MTgwODM4NTIsImlzQWRtaW4iOmZhbHNlLCJpc3MiOiJDYW5ldGEgSURQIFNlcnZlciIsInN1YiI6ImZzLnJvZmlxQGdtYWlsLmNvbSIsInVzZXJVaWQiOiI4ZmJiMTNmYi1hZDc4LTQ3NDctOGJlYy1mN2QyYThkNzAyNWQifQ.G_X9Y2aIu4JYZysYW0l79HDyIBFs1GmohUvJqTRhSwh42Ngt1aD1IwKJ6AohNKxueSz-o0RFz6WS9Ub1khEMYNQjdWMSSD4nfzuOlb7pBVAuyoIvucjnPD6wYti-UwLZF2Puap87fNC9kdQKJ8xcuwerVFo9GuxIJ3-8tE0oClcsOdnqlaqlVSHHrWdQVe6XiOFPzmNSL0vZWuUvfetIMXgbT-MMdRNEXLbcmtHmy4JCwwzMow7es7TZWvVReeOIIxgDZTI_Y2x4qKM1Bkbzs-9UxNJy5O8w4Yuq8UN_slNe1AzaW0aLG4FMktWGVWu0SMJM-4oJ9cwEFYoNw8ac8A',
+      // jwtToken: '',
+      jwtToken: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVkZW50aWFsVHlwZSI6ImJhc2ljIiwiZXhwIjoxNjE4MDg0MTUyLCJpYXQiOjE2MTgwODM4NTIsImlzQWRtaW4iOmZhbHNlLCJpc3MiOiJDYW5ldGEgSURQIFNlcnZlciIsInN1YiI6ImZzLnJvZmlxQGdtYWlsLmNvbSIsInVzZXJVaWQiOiI4ZmJiMTNmYi1hZDc4LTQ3NDctOGJlYy1mN2QyYThkNzAyNWQifQ.G_X9Y2aIu4JYZysYW0l79HDyIBFs1GmohUvJqTRhSwh42Ngt1aD1IwKJ6AohNKxueSz-o0RFz6WS9Ub1khEMYNQjdWMSSD4nfzuOlb7pBVAuyoIvucjnPD6wYti-UwLZF2Puap87fNC9kdQKJ8xcuwerVFo9GuxIJ3-8tE0oClcsOdnqlaqlVSHHrWdQVe6XiOFPzmNSL0vZWuUvfetIMXgbT-MMdRNEXLbcmtHmy4JCwwzMow7es7TZWvVReeOIIxgDZTI_Y2x4qKM1Bkbzs-9UxNJy5O8w4Yuq8UN_slNe1AzaW0aLG4FMktWGVWu0SMJM-4oJ9cwEFYoNw8ac8A',
 
       isDashboard: false, // default value
 
@@ -73,19 +73,6 @@ export default {
 
     }
   },
-  // created() {
-  //   if (this.isDashboard) {
-  //     if (!this.isCreated) {
-  //     this.refreshToken()
-  //     this.isCreated = true
-  //     }
-  //   }
-  // },
-  // updated() {
-  //   if (this.isDashboard) {
-  //     this.refreshToken()
-  //   }
-  // },
   methods: {
     refreshToken() {
         var email = localStorage.getItem('email')
@@ -106,18 +93,17 @@ export default {
         })
     }, 
     logout() {
-      this.refreshToken()
         var authToken = 'Bearer ' + this.jwtToken
-        // var decoded = jwt_decode(this.jwtToken)
-        // var date = new Date()
-        // var timeNowUnix = Math.floor(date.getTime() / 1000)
+        var decoded = jwt_decode(this.jwtToken)
+        var date = new Date()
+        var timeNowUnix = Math.floor(date.getTime() / 1000)
 
-        // this.isLoadingLogoutProcess = true
+        if (timeNowUnix > decoded.exp) {
+            this.refreshToken()
+            return
+        }
 
-        // if (timeNowUnix > decoded.exp) {
-        //     console.log('expired token')
-        //     this.refreshToken()
-        // }
+        this.isLoadingLogoutProcess = true
 
         axios.post('https://api.catena.id/v1/auth/logout', null, {headers: {'content-type': 'application/x-www-form-urlencoded', 'Authorization': authToken}, withCredentials: true})
         .then((response) => {
